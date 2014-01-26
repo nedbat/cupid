d9abf92882d161c864c345f46a5f1f210508258e
Ned Batchelder <ned@nedbatchelder.com>
2014-01-26 13:52:22 -0500

    A little more in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,27 @@
 
 from setuptools import setup
 
+with open("README.rst") as readme:
+    long_description = readme.read()
+
 setup(
     name="Cupid",
     version="0.4",
     description="Cupid SVG figure-drawing",
+    long_description=long_description,
     author="Ned Batchelder",
     author_email="ned@nedbatchelder.com",
     packages=["cupid"],
     install_requires=[
         "svgwrite",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ],
 )
